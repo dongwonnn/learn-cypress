88d5c4d427d46b09669c1c1fa102c10f936c89ef
dongwonnn <kkum9408@gmail.com>
2021-12-12 23:00:29 +0900

    테스트 정리

--- cypress/integration/counter.spec.js
+++ cypress/integration/counter.spec.js
@@ -30,4 +30,43 @@ describe("카운터 앱 테스트", () => {
           .should("eq", String(preValue + 1));
       });
   });
+
+  it("- 버튼을 클릭 시 counter가 1감소한다.", () => {
+    // 최소값이 0 이기 때문에, +1을 진행하고 -1 버튼을 클릭하는 방향으로 작성한다.
+    cy.get(".increase-btn").click();
+
+    cy.get("#value")
+      // 먼저 기존 값 1을 가져오고
+      .invoke("text")
+      .then((value) => {
+        const preValue = Number(value);
+        // - 버튼을 클릭한 다음에
+        cy.get(".decrease-btn").click();
+        // 변화된 값이 기존값의 -1 (= 0)인지 체크
+        cy.get("#value")
+          .invoke("text")
+          .should("eq", String(preValue - 1));
+      });
+  });
+
+  it("+ 버튼을 눌렀을 때 counter가 10이 넘는 경우 더이상 증가하지 못한다. (Max 값이 10)", () => {
+    // 11번 눌렀을 때 10 이면 성공
+    for (let i = 0; i < 11; i++) {
+      cy.get(".increase-btn").click();
+    }
+
+    cy.get("#value").invoke("text").should("eq", "10");
+  });
+
+  it("- 버튼을 눌렀을 때 counter가 0보다 작아지는 경우 더이상 감소하지 못한다. (Min 값이 0)", () => {
+    cy.get(".decrease-btn").click();
+
+    cy.get("#value").invoke("text").should("eq", "0");
+  });
+
+  it("reset 버튼을 누르면 counter가 0으로 초기화된다.", () => {
+    cy.get(".increase-btn").click();
+    cy.get(".reset-btn").click();
+    cy.get("#value").invoke("text").should("eq", "0");
+  });
 });
